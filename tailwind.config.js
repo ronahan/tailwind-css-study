39648f446aec9080200816aabd9b89098214e09e
rona <ronahan0v0@gmail.com>
2024-03-06 09:56:37 +0900

    tailwind css first commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    //templete 파일 경로 설정 
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  theme: {
+    colors: {
+      primary: {
+        100: '#ebf8ff',
+        300: '#90cdf4',
+        500: '#4299e1',
+        700: '#2b6cb0',
+        900: '#2a4365'
+      },
+      secondary: {
+        100: '#fffff0',
+        300: '#faf089',
+        500: '#ecc94b',
+        700: '#b7791f',
+        900: '#744210'
+      }
+    }
+  }
+}
+
